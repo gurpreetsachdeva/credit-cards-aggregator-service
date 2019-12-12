c42ef89dfd6af91aae5dba167dc4cdb2d4b58683
Gurpreet Sachdeva <gurpreet.sachdevagss@gmail.com>
2019-12-12 15:14:29 +0530

    Starter Project downloaded from Spring Website

---
+++ src/main/java/com/github/gurpreetsachdeva/creditcardsaggregatorservice/CreditCardsAggregatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.gurpreetsachdeva.creditcardsaggregatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardsAggregatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardsAggregatorServiceApplication.class, args);
+	}
+
+}
