dc80e90c9972ef09db8fe3d8f8ceed864d610307
Gurpreet Sachdeva <gurpreet.sachdevagss@gmail.com>
2019-12-15 22:50:11 +0530

    Modified readme

--- src/main/java/com/github/gurpreetsachdeva/creditcardsaggregatorservice/model/CreditCardUser.java
+++ src/main/java/com/github/gurpreetsachdeva/creditcardsaggregatorservice/model/CreditCardUser.java
@@ -5,7 +5,7 @@ import io.swagger.annotations.ApiModelProperty;
 public class CreditCardUser {
 
 	@ApiModelProperty(notes = "Credit score between 0 and 700", name = "creditScore", required = true, value = "555")
-	private int creditScore;
+	private Integer creditScore;
 	@ApiModelProperty(notes = "Users full name", name = "name", required = true, value = "Gurpreet Sachdeva")
 	private String name;
 	@ApiModelProperty(notes = "Users annual salary", name = "salary", required = false, value = "100")
@@ -13,18 +13,18 @@ public class CreditCardUser {
 
 
 
-	public CreditCardUser(int creditScore, String name, Integer salary) {
+	public CreditCardUser(Integer creditScore, String name, Integer salary) {
 		super();
 		this.creditScore = creditScore;
 		this.name = name;
 		this.salary = salary;
 	}
 
-	public int getCreditScore() {
+	public Integer getCreditScore() {
 		return creditScore;
 	}
 
-	public void setCreditScore(int creditScore) {
+	public void setCreditScore(Integer creditScore) {
 		this.creditScore = creditScore;
 	}
 
